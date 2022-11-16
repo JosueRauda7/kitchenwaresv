dff179d22fc02f9f507d88e5a2bb0a6add4a4c47
Josue Rauda <katerozarack@gmail.com>
2022-11-16 10:36:22 -0600

    prueba

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,2 @@
-// export const baseUrlDevelopment = "http://localhost:8080/api";
+export const baseUrlDevelopment = "http://localhost:8080/api";
 export const baseUrlProduction = "https://kitchenware-api.onrender.com/api";
