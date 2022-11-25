44dc488f2b1497c472ef5a55eac8a696d3ae0da5
Josue Rauda <katerozarack@gmail.com>
2022-11-25 01:59:16 -0600

    merge con main. nuevo producto, arreglado

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 // DESARROLLO
-// export const baseUrl = "http://localhost:8080/api";
-// export const urlImages = "http://localhost:8080";
+export const baseUrl = "http://localhost:8080/api";
+export const urlImages = "http://localhost:8080";
 
 // PRODUCCION
-export const baseUrl = "https://kitchenware-api.onrender.com/api";
-export const urlImages = "https://kitchenware-api.onrender.com";
+// export const baseUrl = "https://kitchenware-api.onrender.com/api";
+// export const urlImages = "https://kitchenware-api.onrender.com";
